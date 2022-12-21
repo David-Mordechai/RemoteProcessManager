715947e7c499de7442705f03f907a3c5cd386c21
David Mordechai <david.mordechai79@gmail.com>
2022-12-22 00:26:18 +0200

    refactoring settings arguments options

--- RemoteProcessManager/Models/Settings.cs
+++ RemoteProcessManager/Models/Settings.cs
@@ -5,23 +5,23 @@ namespace RemoteProcessManager.Models;
 
 public class Settings
 {
-    [Option('a', "agent-mode", Required = true, HelpText = "Set Agent mode, 1 = Agent, 2 = ProxyAgent")]
+    [Option('m', "agent-mode", Required = true, HelpText = "Set Agent mode, 1 = Agent, 2 = ProxyAgent")]
     public ModeType AgentMode { get; set; } = ModeType.Agent;
 
     [Option('n', "agent-name", Required = true,
         HelpText = "Set Agent Name, for example video1, this name will be use for creating message broker topics")]
     public string AgentName { get; set; } = default!;
 
-    [Option('u', "messageBroker-url", Required = true, HelpText = "Set MessageBroker Url, for example, redis url 127.0.0.1:6379")]
+    [Option('b', "messageBroker-url", Required = true, HelpText = "Set MessageBroker Url, for example, redis url 127.0.0.1:6379")]
     public string MessageBrokerUrl { get; set; } = default!;
 
     [Option('h', "http-port", Required = true, HelpText = "Set Http Port, port for rest api")]
     public ulong HttpPort { get; set; } = default!;
 
-    [Option('p', "process-name", HelpText = "Set remote process fullname")]
+    [Option('p', "process-name", HelpText = "Set remote process fullname. Only set if Agent Mode is ProxyAgent")]
     public string ProcessFullName { get; set; } = default!;
 
-    [Option('p', "process-args", HelpText = "Set remote process arguments")]
+    [Option('a', "process-args", HelpText = "Set remote process arguments. Only set if Agent Mode is ProxyAgent")]
     public string ProcessArguments { get; set; } = default!;
 
     public string StartProcessTopic => $"start_process_{AgentName}";
